2b1ce9b5d0c29e1ba9b1bf42167159757911aecb
Guilherme Toja Camilo <guilherme.camilo@synvia.com>
2025-08-21 11:25:42 -0300

    feat: added some functionality to the gamedeck

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Selecionar o gameNav (parte de cima da tela) e mudamos o texto e o tamanho da fonte
+
 const gameNav = document.querySelector(".mainGameNav");
 gameNav.innerHTML = "Math Like";
 gameNav.style.fontSize = "48px";
+
+// Selecionar as partes principais do jogo, area de jogo, deck e mao
+
+const playArea = document.querySelector("#playArea");
+const playerMainDeck = document.querySelector("#playerMainDeck");
+const playerMainHand = document.querySelector("#playerMainHand");
+
+// Adiciono um evento quando o botao e clicado
+playerMainDeck.addEventListener("click", () => {
+  console.log("clicked");
+});
